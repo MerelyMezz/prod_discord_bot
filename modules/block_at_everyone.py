181737d3903c68fc04c3acf191bca2a039abf899
becky <becky>
2025-03-26 10:10:35 +0100

    changed @everyone detection to a regex, because the mention_everyone flag isnt set if the user doesnt actually have that permission

--- modules/block_at_everyone.py
+++ modules/block_at_everyone.py
@@ -1,11 +1,12 @@
 import prod_api_helpers
+import re
 
 ModuleFunctions = {}
 ModuleFunctions["MESSAGE_CREATE"] = {"BlockAtEveryone"}
 ModuleFunctions["MESSAGE_UPDATE"] = {"BlockAtEveryone"}
 
 def BlockAtEveryone(d):
-    if not d["mention_everyone"] == True:
+    if re.search("@everyone", d["content"]) == None:
         return
 
     prod_api_helpers.QuarantineUser(d["author"]["id"], "Posted an @everyone mention")
